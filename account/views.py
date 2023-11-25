a1f624e1f92ce303553ce5d25d16c1d3cb7cadc0
virendra1485 <virendrasinghrawat1485@gmail.com>
2023-11-25 20:49:02 +0530

    Add view for user registration

--- account/views.py
+++ account/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
+from .form import UserSignUpForm
+from django.contrib.auth import login
 
-# Create your views here.
+
+class UserRegistrationView(CreateView):
+    form_class = UserSignUpForm
+    success_url = "/user/account/log-in/"
+    template_name = "account/registration.html"
+
+    def form_valid(self, form):
+        form.instance.role = "worker"
+        response = super().form_valid(form)
+        self.object.set_password(form.cleaned_data['password'])
+        self.object.save()
+        login(self.request, self.object)
+        return response
