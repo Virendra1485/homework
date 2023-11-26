b3a7f21348a1b74bae00d86c7d83c688e1cfd082
virendra1485 <virendrasinghrawat1485@gmail.com>
2023-11-26 13:19:15 +0530

    Add worker and customer view

--- account/views.py
+++ account/views.py
@@ -1,9 +1,11 @@
+from django.contrib.auth import login, logout
 from django.contrib.auth.views import LoginView
-from django.views.generic import CreateView, View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView, View, DetailView, ListView
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from .form import UserSignUpForm
-from django.contrib.auth import login, logout
+from .models import UserAccount
 
 
 class UserRegistrationView(CreateView):
@@ -29,7 +31,7 @@ class UserLogInView(LoginView):
     def get_success_url(self):
         user = self.request.user
         if user is None:
-            return reverse_lazy("signin")
+            return reverse_lazy("log-in")
         elif user.role == "customer":
             return reverse_lazy('workers') + '?page=1'
         elif user.role == "worker":
@@ -41,4 +43,38 @@ class UserLogInView(LoginView):
 class UserLogOutView(View):
     def get(self, request):
         logout(request)
-        return redirect("signin")
+        return redirect("log-in")
+
+
+class CustomerListView(LoginRequiredMixin, ListView):
+    model = UserAccount
+    template_name = "account/customers.html"
+    context_object_name = "customers"
+    paginate_by = 5
+
+    def get_queryset(self):
+        return UserAccount.objects.filter(role="customer")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+
+class WorkerListView(LoginRequiredMixin, ListView):
+    model = UserAccount
+    template_name = "account/workers.html"
+    context_object_name = "workers"
+    paginate_by = 5
+
+    def get_queryset(self):
+        return UserAccount.objects.filter(role="worker")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+
+class WorkerDetailView(DetailView):
+    model = UserAccount
+    template_name = 'account/worker_detail.html'
+    context_object_name = 'worker'
