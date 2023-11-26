76de6ebacbac94aaa72ea90c6db625d66c705063
virendra1485 <virendrasinghrawat1485@gmail.com>
2023-11-26 12:11:58 +0530

    Ad login logout feature

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,9 @@
-from django.contrib import admin
 from django.urls import path
-from .views import UserRegistrationView
+from .views import UserRegistrationView, UserLogInView, UserLogOutView
 
 
 urlpatterns = [
     path('registration/', UserRegistrationView.as_view(), name="registration"),
+    path('log-in/', UserLogInView.as_view(), name="log-in"),
+    path('log-out/', UserLogOutView.as_view(), name="log-out"),
 ]
